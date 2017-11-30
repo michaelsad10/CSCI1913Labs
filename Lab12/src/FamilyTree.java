f2a1d46d25359c869e3e700084cda349cd92d244
Michael <michaelsad10@gmail.com>
2017-11-29 19:58:57 -0600

    Started

---
+++ Lab12/src/FamilyTree.java
@@ -0,0 +1,45 @@
+public class FamilyTree
+{
+    private class Node
+    {
+        private String name;
+        private Node father;
+        private Node mother;
+
+        private Node(String name, Node father, Node mother)
+        {
+            this.name = name;
+            this.father = father;
+            this.mother = mother;
+        }
+    }
+
+    public FamilyTree(String ego)
+    {
+
+    }
+    private Node find(String name)
+    {
+
+    }
+
+    private Node find(String name, Node root)
+    {
+
+    }
+
+    public void addParents(String ego, String father, String mother)
+    {
+
+    }
+
+    public boolean isDescendant(String ego, String ancestor)
+    {
+
+    }
+
+    private boolean isDescendant(Node root, Node ancestor)
+    {
+
+    }
+}
