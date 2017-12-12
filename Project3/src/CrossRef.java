59ac8876e7e232f20827084f9d46d53dc175a63a
Michael <michaelsad10@gmail.com>
2017-12-12 17:10:56 -0600

    Still working

--- Project3/src/CrossRef.java
+++ Project3/src/CrossRef.java
@@ -44,14 +44,15 @@ class CrossRef
     }
 
 
-    public void addBST(String name, int line) {
+    public void addBST(String name, int line)
+    {
         List last = new List(line, null);
         if (root == null) // If the root is empty
         {
             root = new Node(name, last, last);
         }
         else
-            {
+        {
             Node temp = root;
             while (true)
             {
@@ -93,268 +94,298 @@ class CrossRef
         }
     }
 
+
     public String toString()
     {
-        StringBuilder stringBuilder = new StringBuilder();
-        makeString(stringBuilder,root);
-        return stringBuilder.toString();
+        String temp = "";
+        temp+=inOrder(root,"");
+        return temp;
     }
-    private void makeString(StringBuilder stringBuilder, Node root)
+    public String print(Node root)
+    {
+        String word = "";
+        List head = root.values;
+        while(head!=null)
+        {
+            head = head.next;
+            word+= head.value;
+        }
+        return word;
+    }
+
+    public String inOrder(Node root,String fuckthisProject)
     {
         if(root!=null)
         {
-            makeString(stringBuilder, root.left);
-            stringBuilder.append(root.toString()).append("\n");
-            makeString(stringBuilder, root.right);
+            inOrder(root.left,fuckthisProject);
+            fuckthisProject+= root.name + print(root);
+            inOrder(root.right,fuckthisProject);
         }
-    }
-    public String toString()
-    {
-        StringBuilder temp = new StringBuilder();
-        StringBuilder stringBuilder = new StringBuilder();
-        while(values != null)
-        {
-            temp.append(String.format("%05d", lines.line)).append(" ");
 
-        }
+        return fuckthisProject;
     }
+
 
 }
 
-class Test
-{
-    public static void main(String[] args)
-    {
+class CrossTest {
+    public static void main(String[] args) {
         String path;
-        if(args.length>1)
-        {
-            throw new IllegalArgumentException();
-        }
-        else
-        {
+
+        if (args.length > 1){
+            throw new IllegalArgumentException("Only 1 argument");
+        }else{
             path = args[0];
-            Nomenclator reader = new Nomenclator(path,true);
-            CrossRef test = new CrossRef();
-            while(reader.hasNext())
-            {
-                test.addBST(reader.nextName(),reader.nextNumber());
+            Nomenclator reader = new Nomenclator(path, false);
+            CrossRef crossReference = new CrossRef();
+
+            while (reader.hasNext()) {
+                crossReference.addBST(reader.nextName(), reader.nextNumber());
             }
 
-            System.out.println(test.toString());
+            System.out.println(crossReference.toString());
         }
+
+        /*
+        Factorials                     00015
+        String                         00008 00009 00029
+        System                         00031
+        Test                           00001 00005
+        Test_8                         00006
+        args                           00029
+        class                          00001 00015
+        eleven                         00009
+        else                           00022
+        factorial                      00019 00023 00031
+        for                            00030
+        if                             00020
+        int                            00005 00006 00019 00030
+        k                              00030 00031
+        main                           00029
+        n                              00019 00020 00023
+        out                            00031
+        println                        00031
+        private                        00005 00006 00009 00019
+        public                         00001 00008 00029
+        return                         00021 00023
+        static                         00019 00029
+        ten                            00008
+        void                           00029
+ */
+
     }
 }
 
 
-    /**
-     * Correct version of Nomenclator
-     */
-    class Nomenclator {
-        private char ch;                 //  Current CHAR from READER.
-        private static final char eof = (char) 0x00;  //  End of file sentinel.
-        private static final char eol = (char) 0x0A;  //  End of line sentinel.
-        private int index;              //  Index into LINE.
-        private String line;               //  Current LINE from READER.
-        private boolean listing;            //  Are we listing the file?
-        private String name;               //  Current name.
-        private int number;             //  Current line number.
-        private String path;               //  Pathname to READER's file.
-        private BufferedReader reader;             //  Read CHARs from here.
+/**
+ * Correct version of Nomenclator
+ */
+class Nomenclator {
+    private char ch;                 //  Current CHAR from READER.
+    private static final char eof = (char) 0x00;  //  End of file sentinel.
+    private static final char eol = (char) 0x0A;  //  End of line sentinel.
+    private int index;              //  Index into LINE.
+    private String line;               //  Current LINE from READER.
+    private boolean listing;            //  Are we listing the file?
+    private String name;               //  Current name.
+    private int number;             //  Current line number.
+    private String path;               //  Pathname to READER's file.
+    private BufferedReader reader;             //  Read CHARs from here.
 
 //  Constructor. Initialize a new NOMENCLATOR that reads from a text file whose
 //  pathname is PATH. If we can't open it then throw an exception. LISTING says
 //  whether we should copy the file to standard output as we read it.
 
-        public Nomenclator(String path, boolean listing) {
-            try {
-                index = 0;
-                line = "";
-                this.listing = listing;
-                number = 0;
-                this.path = path;
-                reader = new BufferedReader(new FileReader(path));
-                skipChar();
-            } catch (IOException ignore) {
-                throw new IllegalArgumentException("Can't open '" + path + "'.");
-            }
+    public Nomenclator(String path, boolean listing) {
+        try {
+            index = 0;
+            line = "";
+            this.listing = listing;
+            number = 0;
+            this.path = path;
+            reader = new BufferedReader(new FileReader(path));
+            skipChar();
+        } catch (IOException ignore) {
+            throw new IllegalArgumentException("Can't open '" + path + "'.");
         }
+    }
 
 //  HAS NEXT. Test if there's another name waiting to be read. If so, then read
 //  it, so NEXT NAME and NEXT NUMBER can return it and its line number later.
 
-        public boolean hasNext() {
-            while (true) {
-                if (Character.isJavaIdentifierStart(ch)) {
-                    skipName();
-                    return true;
-                } else if (Character.isDigit(ch)) {
-                    skipNumber();
-                } else {
-                    switch (ch) {
-                        case eof: {
-                            return false;
-                        }
-                        case '"':
-                        case '\'': {
-                            skipDelimited();
-                            break;
-                        }
-                        case '/': {
-                            skipComment();
-                            break;
-                        }
-                        default: {
-                            skipChar();
-                            break;
-                        }
+    public boolean hasNext() {
+        while (true) {
+            if (Character.isJavaIdentifierStart(ch)) {
+                skipName();
+                return true;
+            } else if (Character.isDigit(ch)) {
+                skipNumber();
+            } else {
+                switch (ch) {
+                    case eof: {
+                        return false;
+                    }
+                    case '"':
+                    case '\'': {
+                        skipDelimited();
+                        break;
+                    }
+                    case '/': {
+                        skipComment();
+                        break;
+                    }
+                    default: {
+                        skipChar();
+                        break;
                     }
                 }
             }
         }
+    }
 
 //  NEXT NAME. If HAS NEXT was true, then return the next name. If HAS NEXT was
 //  false, then return an undefined string.
 
-        public String nextName() {
-            return name;
-        }
+    public String nextName() {
+        return name;
+    }
 
 //  NEXT NUMBER. If HAS NEXT was true, then return the line number on which the
 //  next name appears. If HAS NEXT was false, then return an undefined INT.
 
-        public int nextNumber() {
-            return number;
-        }
+    public int nextNumber() {
+        return number;
+    }
 
 //  SKIP CHAR. If no more CHARs remain unread in LINE, then read the next line,
 //  adding an EOL at the end. If no lines can be read, then read a line with an
 //  EOF char in it. Otherwise just read the next char from LINE and return it.
 
-        private void skipChar() {
-            if (index >= line.length()) {
-                index = 0;
-                number += 1;
-                try {
-                    line = reader.readLine();
-                    if (line == null) {
-                        line = "" + eof;
-                    } else {
-                        if (listing) {
-                            System.out.format("%05d ", number);
-                            System.out.println(line);
-                        }
-                        line += eol;
-                    }
-                } catch (IOException ignore) {
+    private void skipChar() {
+        if (index >= line.length()) {
+            index = 0;
+            number += 1;
+            try {
+                line = reader.readLine();
+                if (line == null) {
                     line = "" + eof;
+                } else {
+                    if (listing) {
+                        System.out.format("%05d ", number);
+                        System.out.println(line);
+                    }
+                    line += eol;
                 }
+            } catch (IOException ignore) {
+                line = "" + eof;
             }
-            ch = line.charAt(index);
-            index += 1;
         }
+        ch = line.charAt(index);
+        index += 1;
+    }
 
 //  SKIP COMMENT. We end up here if we read a '/'. If it is followed by another
 //  '/', or by a '*', then we skip a comment. We must skip comments so that any
 //  names that appear in them will be ignored.
 
-        private void skipComment() {
+    private void skipComment() {
+        skipChar();
+        if (ch == '*') {
             skipChar();
-            if (ch == '*') {
-                skipChar();
-                while (true) {
-                    if (ch == '*') {
+            while (true) {
+                if (ch == '*') {
+                    skipChar();
+                    if (ch == '/') {
                         skipChar();
-                        if (ch == '/') {
-                            skipChar();
-                            return;
-                        }
-                    } else if (ch == eof) {
                         return;
-                    } else {
-                        skipChar();
                     }
+                } else if (ch == eof) {
+                    return;
+                } else {
+                    skipChar();
                 }
-            } else if (ch == '/') {
-                skipChar();
-                while (true) {
-                    if (ch == eof) {
-                        return;
-                    } else if (ch == eol) {
-                        skipChar();
-                        return;
-                    } else {
-                        skipChar();
-                    }
+            }
+        } else if (ch == '/') {
+            skipChar();
+            while (true) {
+                if (ch == eof) {
+                    return;
+                } else if (ch == eol) {
+                    skipChar();
+                    return;
+                } else {
+                    skipChar();
                 }
             }
         }
+    }
 
 //  SKIP DELIMITED. Skip a string constant or a character constant, so that any
 //  names that appear inside them will be ignored.  Throw an exception if there
 //  is a missing delimiter at the end.
 
-        private void skipDelimited() {
-            char delimiter = ch;
-            skipChar();
-            while (true) {
-                if (ch == delimiter) {
-                    skipChar();
-                    return;
-                } else {
-                    switch (ch) {
-                        case eof:
-                        case eol: {
+    private void skipDelimited() {
+        char delimiter = ch;
+        skipChar();
+        while (true) {
+            if (ch == delimiter) {
+                skipChar();
+                return;
+            } else {
+                switch (ch) {
+                    case eof:
+                    case eol: {
+                        throw new IllegalStateException("Bad string in '" + path + "'.");
+                    }
+                    case '\\': {
+                        skipChar();
+                        if (ch == eol || ch == eof) {
                             throw new IllegalStateException("Bad string in '" + path + "'.");
-                        }
-                        case '\\': {
+                        } else {
                             skipChar();
-                            if (ch == eol || ch == eof) {
-                                throw new IllegalStateException("Bad string in '" + path + "'.");
-                            } else {
-                                skipChar();
-                            }
-                            break;
                         }
-                        default: {
-                            skipChar();
-                            break;
-                        }
+                        break;
+                    }
+                    default: {
+                        skipChar();
+                        break;
                     }
                 }
             }
         }
+    }
 
 //  SKIP NAME. Skip a name, but convert it to a STRING, stored in NAME.
 
-        private void skipName() {
-            StringBuilder builder = new StringBuilder();
-            while (Character.isJavaIdentifierPart(ch)) {
-                builder.append(ch);
-                skipChar();
-            }
-            name = builder.toString();
+    private void skipName() {
+        StringBuilder builder = new StringBuilder();
+        while (Character.isJavaIdentifierPart(ch)) {
+            builder.append(ch);
+            skipChar();
         }
+        name = builder.toString();
+    }
 
 //  SKIP NUMBER. Skip something that might be a number. It starts with a digit,
 //  followed by zero or more letters and digits. We must do this so the letters
 //  aren't treated as names.
 
-        private void skipNumber() {
+    private void skipNumber() {
+        skipChar();
+        while (Character.isJavaIdentifierPart(ch)) {
             skipChar();
-            while (Character.isJavaIdentifierPart(ch)) {
-                skipChar();
-            }
         }
+    }
 
 //  MAIN. Get a file pathname from the command line. Read a series of names and
 //  their line numbers from the file, and write them one per line. For example,
 //  the command "java Nomenclator Nomenclator.java" reads names from the source
 //  file you are now looking at. This method is only for debugging!
 
-        public static void main(String[] args) {
-            Nomenclator reader = new Nomenclator(args[0], false);
-            while (reader.hasNext()) {
-                System.out.println(reader.nextNumber() + " " + reader.nextName());
-            }
+    public static void main(String[] args) {
+        Nomenclator reader = new Nomenclator(args[0], false);
+        while (reader.hasNext()) {
+            System.out.println(reader.nextNumber() + " " + reader.nextName());
         }
     }
+}
